e8859c7326fd61f5372c9047d78cb90214e39e72
marcelooliveira <mclricardo@gmail.com>
2019-02-06 15:41:51 -0200

    .

--- Programa03/Program.cs
+++ Programa03/Program.cs
@@ -15,56 +15,6 @@ namespace Programa03
             Relatorio relatorio = new Relatorio("Relatório de Vendas");
             relatorio.Imprimir();
 
-            Type type = relatorio.GetType();
-            Console.WriteLine("Tipo de relatorio é : {0}", type.ToString());
-
-            Console.WriteLine("Os membros de relatório são:");
-            foreach (System.Reflection.MemberInfo memberInfo in type.GetMembers())
-            {
-                Console.WriteLine(memberInfo.ToString());
-            }
-
-            relatorio.Nome = "NOME MODIFICADO!";
-            relatorio.Imprimir();
-
-            MethodInfo methodInfo = type.GetMethod("set_Nome");
-            methodInfo.Invoke(relatorio, new object[] { "NOME MODIFICADO POR REFLECTION" });
-            relatorio.Imprimir();
-
-            Console.WriteLine("Tipos do Assembly:");
-            Console.WriteLine("==================");
-            Assembly esteAssembly = Assembly.GetExecutingAssembly();
-            Type[] tipos = esteAssembly.GetTypes();
-            foreach (var tipo in tipos)
-            {
-                Console.WriteLine(tipo.ToString());
-
-                if (tipo.IsInterface)
-                    continue;
-
-                if (typeof(IRelatorio).IsAssignableFrom(tipo))
-                {
-                    Console.WriteLine("Tipo {0} implementa a interface IRelatorio", tipo.ToString());
-                }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Investigando tipos via LINQ");
-
-            var tiposRelatorio =
-                from t in tipos
-                where !t.IsInterface
-                    && typeof(IRelatorio).IsAssignableFrom(t)
-                select t;
-
-            Console.WriteLine();
-            Console.WriteLine("Tipos que implementam a interface IRelatorio:");
-            Console.WriteLine();
-            foreach (var tipo in tiposRelatorio)
-            {
-                Console.WriteLine(tipo.ToString());
-            }
-
             Console.WriteLine();
             if (Attribute.IsDefined(typeof(Venda), typeof(SerializableAttribute)))
             {
@@ -73,6 +23,75 @@ namespace Programa03
             else
             {
                 Console.WriteLine("A classe Venda NÃO DEFINE o atributo Serializable");
+            }
+            Console.WriteLine();
+
+            //TAREFA 1: 
+            Console.WriteLine("OBTENDO O TIPO DO RELATÓRIO");
+            Console.WriteLine("===========================");
+
+            Type type = relatorio.GetType();
+            Console.WriteLine(type.ToString());
+
+            //TAREFA 2: 
+            Console.WriteLine();
+            Console.WriteLine("OBTENDO OS MEMBROS DO RELATÓRIO");
+            Console.WriteLine("===============================");
+            Console.WriteLine();
+
+            foreach (System.Reflection.MemberInfo memberInfo in type.GetMembers())
+            {
+                Console.WriteLine(memberInfo.ToString());
+            }
+
+            //TAREFA 3: 
+            Console.WriteLine();
+            Console.WriteLine("MODIFICANDO NOME VIA REFLECTION");
+            Console.WriteLine("===============================");
+            Console.WriteLine();
+
+            relatorio.Nome = "NOME MODIFICADO!";
+            relatorio.Imprimir();
+
+            MethodInfo methodInfo = type.GetMethod("set_Nome");
+            methodInfo.Invoke(relatorio, new object[] { "NOME MODIFICADO POR REFLECTION" });
+            relatorio.Imprimir();
+
+            //TAREFA 4: 
+            //     E 5: 
+
+            Console.WriteLine();
+            Console.WriteLine("TIPOS DO ASSEMBLY");
+            Console.WriteLine("QUE IMPLEMENTAM A INTERFACE IRelatorio");
+            Console.WriteLine("======================================");
+            Assembly esteAssembly = Assembly.GetExecutingAssembly();
+            Type[] tipos = esteAssembly.GetTypes();
+            foreach (var tipo in tipos)
+            {
+                if (tipo.IsInterface)
+                    continue;
+
+                if (typeof(IRelatorio).IsAssignableFrom(tipo))
+                {
+                    Console.WriteLine(tipo.ToString());
+                }
+            }
+
+            //TAREFA 6: 
+            Console.WriteLine();
+            Console.WriteLine("USANDO LINQ PARA VER TIPOS DO ASSEMBLY");
+            Console.WriteLine("QUE IMPLEMENTAM A INTERFACE IRelatorio");
+            Console.WriteLine("======================================");
+
+            var tiposRelatorio =
+                from t in tipos
+                where !t.IsInterface
+                    && typeof(IRelatorio).IsAssignableFrom(t)
+                select t;
+
+            foreach (var tipo in tiposRelatorio)
+            {
+                Console.WriteLine(tipo.ToString());
             }
 
             Console.ReadLine();
