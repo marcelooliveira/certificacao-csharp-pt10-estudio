48813b2e206c0ed9d73e1249269d6a827e811915
marcelooliveira <mclricardo@gmail.com>
2019-02-07 13:32:36 -0200

    .

--- Programa04/Program.cs
+++ Programa04/Program.cs
@@ -14,36 +14,54 @@ namespace Programa04
     {
         static void Main(string[] args)
         {
+            //TAREFA: Utilizar código C# para gerar código C#,
+            //          produzindo a classe Funcionario:
+
+            /*
+            namespace RecursosHumanos
+            {
+                using system;
+                public class Funcionario
+                {
+                    public string nome;
+                    public decimal salario;
+                    public Funcionario(string nome, decimal salario)
+                    {
+                    }
+                }
+            }
+            */
+
+            //Tarefa 1: criar uma unidade de compilação
             CodeCompileUnit codeCompileUnit = new CodeCompileUnit();
 
-            //define um novo namespace de Funcionario
+            //Tarefa 2: criar o namespace RecursosHumanos
             CodeNamespace nameSpace = new CodeNamespace("RecursosHumanos");
 
-            //importa o namespace System
+            //Tarefa 2.1: importar o namespace System
             CodeNamespaceImport import = new CodeNamespaceImport("system");
             nameSpace.Imports.Add(import);
 
-            //cria classe Funcionario
+            //Tarefa 2.2: criar a classe Funcionario
             CodeTypeDeclaration classe = new CodeTypeDeclaration("Funcionario");
             classe.IsClass = true;
             classe.TypeAttributes = System.Reflection.TypeAttributes.Public;
 
-
-            //cria o campo nome
+            //Tarefa 2.3: criar o campo nome
             CodeMemberField campoNome = new CodeMemberField(typeof(string), "nome");
             campoNome.Attributes = MemberAttributes.Public;
 
             //adiciona o campo à classe
             classe.Members.Add(campoNome);
 
-            //cria o campo salário
+            //Tarefa 2.4: criar o campo salário
             CodeMemberField campoSalario = new CodeMemberField(typeof(decimal), "salario");
             campoSalario.Attributes = MemberAttributes.Public;
 
             //adiciona o campo à classe
             classe.Members.Add(campoSalario);
 
-            //cria o construtor da classe
+            //Tarefa 2.5: criar o construtor da classe
             CodeConstructor constructor = new CodeConstructor();
             constructor.Attributes = MemberAttributes.Public;
             constructor.Parameters.Add(new CodeParameterDeclarationExpression(typeof(string), "nome"));
@@ -58,10 +76,10 @@ namespace Programa04
             //adiciona o namespace ao documento
             codeCompileUnit.Namespaces.Add(nameSpace);
 
-            //Cria o provedor de modelo de código
+            //Tarefa 3: cria o provedor de modelo de código
             CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
 
-            //processa o documento
+            //Tarefa 4: gerar código e salva o arquivo
             using (var sw = new StreamWriter("Funcionario.cs"))
             {
                 CodeGeneratorOptions options = new CodeGeneratorOptions();
