69cbe3fa9ddf3c0da71a08338fc40baadd0aaa8f
marcelooliveira <mclricardo@gmail.com>
2019-02-08 13:33:09 -0200

    .

--- Programa05/Program.cs
+++ Programa05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 namespace Programa05
 {
@@ -6,33 +7,43 @@ namespace Programa05
     {
         static void Main(string[] args)
         {
+            TextWriterTraceListener consoleListener = new TextWriterTraceListener(Console.Out);
+            Trace.Listeners.Add(consoleListener);
+
+            //Tarefa 1: obter o nome completo do assembly atual
             Assembly assembly = Assembly.GetExecutingAssembly();
-            Console.WriteLine("Nome completo: {0}", assembly.FullName);
-            AssemblyName name = assembly.GetName();
-            Console.WriteLine("Versão Major: {0}", name.Version.Major);
-            Console.WriteLine("Versão Minor: {0}", name.Version.Minor);
-            Console.WriteLine("Está no Global Assembly Cache? {0}", assembly.GlobalAssemblyCache);
-            foreach (Module assemblyModule in assembly.Modules)
+            Trace.WriteLine($"Nome completo: {assembly.FullName}");
+
+            //Tarefa 2: obter a identificação do assembly atual
+            AssemblyName identidadeAssembly = assembly.GetName();
+            Trace.WriteLine($"Versão Major: {identidadeAssembly.Version.Major}");
+            Trace.WriteLine($"Versão Minor: {identidadeAssembly.Version.Minor}");
+
+            //Tarefa 3: descobrir se o assembly atual está no Global Assembly Cache
+            Trace.WriteLine($"Está no Global Assembly Cache? {assembly.GlobalAssemblyCache}");
+
+            //Tarefa 4: descobrir todos os módulos, tipos e membros do assembly
+            foreach (Module modulo in assembly.Modules)
             {
-                Console.WriteLine(" Módulo: {0}", assemblyModule.Name);
-                foreach (Type moduleType in assemblyModule.GetTypes())
+                Trace.Indent();
+                Trace.WriteLine($"Módulo: {modulo.Name}");
+                foreach (Type tipoModulo in modulo.GetTypes())
                 {
-                    Console.WriteLine(" Tipo: {0}", moduleType.Name);
-                    foreach (MemberInfo member in moduleType.GetMembers())
+                    Trace.Indent();
+                    Trace.WriteLine($"Tipo: {tipoModulo.Name}");
+                    foreach (MemberInfo membro in tipoModulo.GetMembers())
                     {
-                        Console.WriteLine(" Membro: {0}", member.Name);
+                        Trace.Indent();
+                        Trace.WriteLine($"Membro: {membro.Name} ({membro.MemberType})");
+                        Trace.Unindent();
                     }
+                    Trace.Unindent();
                 }
+                Trace.Unindent();
             }
 
             Console.ReadKey();
         }
-    }
-
-    public class Person
-    {
-        public String Name { get; set; }
-        public String Age { get; }
     }
 }
 
